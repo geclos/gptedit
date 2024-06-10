aae225a7722571d790d64fd3db5d63f45283f4a9
Gerard Clos <clos.gerard@gmail.com>
2024-06-11 00:15:27 +0200

    first mvp done

--- init.lua
+++ init.lua
@@ -1,24 +1,47 @@
 -- Required libraries
-local uv = vim.loop
 local api = vim.api
 local curl = require("plenary.curl")
-
-local function get_current_buffer_and_position()
-  local mode = api.nvim_get_mode().mode
-  local row, col = unpack(api.nvim_win_get_cursor(0))
-  local buffer = api.nvim_buf_get_lines(0, 0, -1, false)
-  local filetype = vim.bo.filetype
-  return buffer, row, col, filetype
-end
 
 -- Read user input command
 local function prompt_user_input(prompt)
   return vim.fn.input(prompt)
 end
 
+-- @param user_input string: The input provided by the user.
+-- @param filetype string: The type of the file where the code resides (e.g., "lua").
+-- @param buffer table: The buffer containing the entire content of the file.
+-- @param row number: The current row position of the cursor.
+-- @param selection table|nil: The selected lines in the buffer, if any.
+local function build_user_prompt(user_input, filetype, buffer, row, selection)
+  local prompt = "I have the following code:\n\n"
+  .. "```" .. filetype .. "\n" .. table.concat(buffer, "\n") .. "\n```\n\n"
+  .. "Cursor is at line: \n\n"
+  .. "```" .. filetype .. "\n" .. table.concat(api.nvim_buf_get_lines(0, row-1, row, false), "\n") .. "\n```\n\n"
+
+  if selection then
+    prompt = prompt
+    .. "I also have the following code selected from the buffer:\n\n"
+  .. "```" .. filetype .. "\n" .. table.concat(selection, "\n") .. "\n```\n\n"
+  end
+
+  prompt = prompt .. user_input .. "\n\n"
+
+  if selection then
+    prompt = prompt
+    .. "Just give me the code snippet to replace my selection, nothing else."
+  else
+    prompt = prompt
+    .. "Just give me the code snippet to add before the cursor, nothing else."
+  end
+
+  return prompt
+end
+
 -- Function to handle streaming from OpenAI API
-local function stream_openai_api(prompt, filetype, buffer, row, column, on_data, on_complete)
+local function stream_openai_api(prompt, filetype, buffer, row, selection)
   local api_key = os.getenv("OPENAI_API_KEY")
+  local user_prompt = build_user_prompt(prompt, filetype, buffer, row, selection)
+
   local body = vim.fn.json_encode({
     model = "gpt-4o",
     messages = {
@@ -31,19 +54,15 @@ local function stream_openai_api(prompt, filetype, buffer, row, column, on_data,
       },
       {
         role = "user",
-        content = "I have the following code:"
-        .. "\n\n```" .. filetype .. "\n" .. table.concat(buffer, "\n") .. "\n```"
-        .. "\n\n Cursor is at line " .. row .. " and column " .. column .. "."
-        .. "\n\n " .. prompt
-        .. "\n\nRespond exclusively with the snippet that should be prepended before the selection above.",
+        content = user_prompt
       }
     },
     n = 1,
     temperature = 0.7,
-    stream = true,
+    stream = false,
   })
 
-  curl.request({
+  local response = curl.request({
     method = 'POST',
     url = 'https://api.openai.com/v1/chat/completions',
     body = body,
@@ -51,89 +70,37 @@ local function stream_openai_api(prompt, filetype, buffer, row, column, on_data,
       ["Content-Type"] = "application/json",
       ["Authorization"] = "Bearer " .. api_key
     },
-    stream = vim.schedule_wrap(function (err, chunk)
-      print('err: ', err)
-      print('chunk: ', chunk)
-      if not chunk or #chunk == 0 then
-        return
-      end
-      if chunk:match("%[DONE%]") then
-        on_complete(partial_line)
-        return
-      end
-      local decoded = vim.json.decode(chunk:match("^data: (.+)$"))
-      if decoded and decoded.choices and decoded.choices[1] and decoded.choices[1].delta then
-        local text_output = decoded.choices[1].delta.content
-        if text_output then
-          on_data(text_output)
-        end
-      end
-    end),
   })
--- The file type of the current buffer is also retrieved.
+
+  return vim.json.decode(response.body)
 end
 
 -- Main function connecting all parts
-local function handle_openai_completion(append, start_line, end_line, range)
-  local buffer, row, col, filetype = get_current_buffer_and_position()
+local function handle_openai_completion(start_line, end_line, range)
+  local filetype = vim.bo.filetype
+  local buffer = api.nvim_buf_get_lines(0, 0, -1, false)
   local user_input = prompt_user_input("Prompt: ")
-
-  vim.notify("Starting stream...", "info", { title = "GPTedit" })
-
-  local last_incomplete_line = ""
--- The buffer contents are fetched from the start to the end of the buffer.
-
-  local function on_data(chunk)
-    local lines = vim.split(last_incomplete_line .. chunk, "\n", true)
-
-    -- Last line might be incomplete, so we store it to be prepended to the next chunk.
-    last_incomplete_line = table.remove(lines)
-    
-    if range == 2 and start_line and end_line then
-      api.nvim_buf_set_lines(0, start_line - 1, end_line, false, lines)
-    else
-      if append then
-        api.nvim_buf_set_lines(0, row, row, false, lines)
-        row = row + #lines
-      else
--- It uses Neovim's API to get the mode, the cursor position (row and column), and the buffer lines.
-        api.nvim_buf_set_lines(0, row - 1, row - 1, false, lines)
-        row = row - 1 + #lines
-      end
-    end
+  local selection
+  if range == 2 then
+    selection = api.nvim_buf_get_lines(0, start_line - 1, end_line, false)
   end
 
-  local function on_complete(last_chunk)
-    if last_chunk ~= "" then
-      local lines = { last_chunk }  -- The final "incomplete line" here is now completed.
-      if range == 2 and start_line and end_line then
-        api.nvim_buf_set_lines(0, start_line - 1, end_line, false, lines)
-      else
-        if append then
-          api.nvim_buf_set_lines(0, row, row, false, lines)
-        else
-          api.nvim_buf_set_lines(0, row - 1, row - 1, false, lines)
-        end
-      end
-    end
-
-    print("DONE")
+  local payload = stream_openai_api(user_input, filetype, buffer, start_line, selection)
+  local msg = payload.choices[1].message.content
+  local lines = {}
+  for line in msg:gmatch("[^\r\n]+") do
+    table.insert(lines, line)
   end
 
-  stream_openai_api(user_input, filetype, buffer, row, col, on_data, on_complete)
+  if range == 2 then
+    api.nvim_buf_set_lines(0, start_line-1, end_line, false, lines)
+  else
+    api.nvim_buf_set_lines(0, start_line-1, start_line-1, false, lines)
+  end
 end
 
--- This function retrieves the current buffer contents, the cursor position, and the file type.
--- Defining command in Neovim
 vim.api.nvim_create_user_command(
-"GPTappend",
-function(params) handle_openai_completion(true, params.line1, params.line2, params.range) end,
-{ nargs = 0, range = true}
-)
--- The file type of the current buffer is also retrieved.
-
-vim.api.nvim_create_user_command(
-"GPTprepend",
-function(params) handle_openai_completion(false, params.line1, params.line2, params.range) end,
+"GPTedit",
+function(params) handle_openai_completion(params.line1, params.line2, params.range) end,
 { nargs = 0, range = true}
 )
